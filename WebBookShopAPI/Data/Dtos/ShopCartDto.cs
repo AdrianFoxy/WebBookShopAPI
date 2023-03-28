63d2f341b8c7a7b7c634a2271663e73b4fa1e1da
AdrianFoxy <drugakadrian@gmail.com>
2023-03-28 09:39:13 +0300

    UpdateDockerAndRedis

--- WebBookShopAPI/Data/Dtos/ShopCartDto.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace WebBookShopAPI.Data.Dtos
-{
-    public class ShopCartDto
-    {
-        public List<ShoppingCartItemDto> ShoppingCartItemDto { get; set; }
-        public double ShopCartTotal { get; set; }
-        public int TotalItems { get; set; }
-    }
-}
